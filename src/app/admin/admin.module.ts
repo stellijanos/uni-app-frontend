ce287b869d744672f707d1f94811c81224092231
Janos Stelli <stellijanos23@gmail.com>
2024-04-08 20:33:09 +0300

    fix project

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
